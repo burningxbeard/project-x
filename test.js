41c9e795f3abc083d0882d89c4b5b5786a3fca07
Chris Braid <chrisbraid.dev@gmail.com>
2024-06-19 17:31:28 -0400

    less testing more testing

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 console.log('First console dot log')
 const fetchDisney = async () => {
-    const url = 'https://api.disneyapi.dev/character?page=30?pageSize=10';
+    const url = 'https://api.disneyapi.dev/character?page=30&pageSize=5';
     const response = await fetch(url);
     const disney = await response.json();
     const character = disney.data.map((data, index) => ({
         name: data.name,
         id: index + 1,
         image: data.imageUrl,
+        url: url,
     }));
     console.log(character)
     
