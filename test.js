abbb0abea738363ffd6f10d8eedc28b792a45c10
Chris Braid <chrisbraid.dev@gmail.com>
2024-06-19 17:26:20 -0400

    testing the testies

--- test.js
+++ test.js
@@ -1,68 +1,15 @@
-const disneydex = document.getElementById('disneydex');
-const cachedPokemon = {};
-
-const fetchPokemon = async () => {
-    const url = `https://api.disneyapi.dev/character?page=10?pageSize=10`;
-    const res = await fetch(url);
-    const disney = await res.json();
-    const pokemon = disney.data.map((data, index) => ({
+console.log('First console dot log')
+const fetchDisney = async () => {
+    const url = 'https://api.disneyapi.dev/character?page=30?pageSize=10';
+    const response = await fetch(url);
+    const disney = await response.json();
+    const character = disney.data.map((data, index) => ({
         name: data.name,
         id: index + 1,
-        image: data.imageUrl
+        image: data.imageUrl,
     }));
-   
-    displayPokemon(pokemon);
-    console.log(pokemon);
-};
-const displayPokemon = (pokemon) => {
-    const pokemonHTMLString = pokemon
-        .map(
-            (pokeman) =>
-                `
-    <li class="card" onclick="selectPokemon(${pokeman.id})">
-        <img class="card-image" src="${pokeman.image}"/>
-        <h2 class="card-title">${pokeman.name}</h2>
-        </a>
-    </li>
-        `
-        )
-        .join('');
-    disneydex.innerHTML = pokemonHTMLString;
+    console.log(character)
+    
 };
 
-const selectPokemon = async (id) => {
-    if (!cachedPokemon[id]) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-        const res = await fetch(url);
-        const pokeman = await res.json();
-        cachedPokemon[id] = pokeman;
-        displayPokemanPopup(pokeman);
-    } else {
-        displayPokemanPopup(cachedPokemon[id]);
-    }
-};
-
-const displayPokemanPopup = (pokeman) => {
-    // console.log(pokeman);
-    const type = pokeman.types.map((type) => type.type.name).join(', ');
-    const htmlString = `
-        <div class="popup">
-            <button id="closeBtn" onclick="closePopup()">X</button>
-            <div class="details">
-                <img class="card-image" src="${
-                    pokeman.sprites['front_default']
-                }"/>
-                <h2 class="card-title">${pokeman.name}</h2>
-                <p><span class="type-pill">${type}</span> | Height:</small> ${pokeman.height} | Weight: ${pokeman.weight}</p>
-            </div>
-        </div>
-    `;
-    disneydex.innerHTML = htmlString + disneydex.innerHTML;
-};
-
-const closePopup = () => {
-    const popup = document.querySelector('.popup');
-    popup.parentElement.removeChild(popup);
-};
-
-fetchPokemon();
+fetchDisney();
